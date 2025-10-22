ce1b98b8cc380f50626f582b9261d031ab2cbf19
Seris355 <retyokopka@gmail.com>
2025-10-22 19:43:24 +0300

    initial commit

---
+++ WEB-lab_2/my-app/src/pages/404/404.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './404.css';
+import Header from '../../components/Header/Header.js';
+import Footer from '../../components/Footer/Footer.js';
+import '../Index/style.css'; 
+import '../../components/Header/header_and_footer_styles.css';
+
+const NotFound = () => {
+  const handleMouseMove = (e) => {
+    const spotlight = document.getElementById('spotlight');
+    if (spotlight) {
+      spotlight.style.background = `radial-gradient(circle at ${e.clientX}px ${e.clientY}px, transparent 100px, rgba(0, 0, 0, 0.95) 150px)`;
+    }
+  };
+
+  return (
+    <div>
+      <Header />
+      <main className="page-container nf-page-container" onMouseMove={handleMouseMove}>
+        <div id="spotlight" className="spotlight" />
+        <section className="H1_sec">
+          <div className="H1_scroll">
+            <h1 className="nf-title">404</h1>
+            <p className="text_main_huge">Страница не найдена</p>
+            <p className="nf-description">Такой страницы не существует</p>
+          </div>
+        </section>
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default NotFound;
---
+++ WEB-lab_2/my-app/src/pages/Service/Service.js
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from 'react';
+import Header from '../../components/Header/Header.js';
+import Footer from '../../components/Footer/Footer.js';
+import servicesData from '../../data/services.json';
+import './service_style.css'; 
+
+const Services = () => {
+  const cards = [
+    { id: 1, image: "/images/images_for_service/image1.png", className: "price_card1" },
+    { id: 2, image: "/images/images_for_service/image2.png", className: "price_card2" },
+    { id: 3, image: "/images/images_for_service/image3.png", className: "price_card3" },
+  ];
+
+  return (
+    <div>
+    <main>
+      <Header />
+      <section className="services">
+        <h2 className="h2_underh">Цены на услуги</h2>
+        <div className="services_section">
+          {cards.map((card) => (
+            <div key={card.id} className={card.className}>
+              <img src={card.image} alt="Наши услуги" className={`service_img img${card.id}`} />
+              <div className={`service_price service_price${card.id}`}>
+                {servicesData
+                  .filter((service) => {
+                    if (card.id === 1) return service.id <= 4;
+                    if (card.id === 2) return service.id > 4 && service.id <= 8;
+                    if (card.id === 3) return service.id > 8;
+                    return false;
+                  })
+                  .map((service) => (
+                    <div key={service.id}>
+                      <h3 className="flex_price">
+                        <span>{service.name}</span>
+                        <span className="price_money">{service.price}</span>
+                      </h3>
+                      <p className="services">{service.description}</p>
+                      <hr />
+                    </div>
+                  ))}
+              </div>
+            </div>
+          ))}
+        </div>
+      </section>
+    </main>
+    <Footer />
+    </div>
+  );
+};
+
+export default Services;
