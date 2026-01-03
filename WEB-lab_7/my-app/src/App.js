7fc8ae5922d9f597fd9a113665544a12c4fb8787
Seris355 <retyokopka@gmail.com>
2026-01-03 19:40:53 +0300

    initial commit

---
+++ WEB-lab_7/my-app/src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from './store/index.js';
+import Index from './pages/Index/Index.js';
+import Contact from './pages/Contact/Contact.js';
+import Our_team from './pages/Our_team/Our_team.js';
+import Service from './pages/Service/Service.js';
+import MasterClass from './pages/MasterClass/MasterClass.jsx';
+import NotFound from './pages/404/404.js'
+import './pages/Index/style.css'; 
+
+function App() {
+  return (
+    <Provider store={store}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/our_team" element={<Our_team />} />
+          <Route path="/service" element={<Service />} />
+          <Route path="/masterclass" element={<MasterClass />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
+    </Provider>
+  );
+}
+
+export default App;
---
+++ WEB-lab_7/my-app/src/pages/Our_team/Our_team.js
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from 'react';
+import Header from '../../components/Header/Header.js';
+import Footer from '../../components/Footer/Footer.js';
+import teamData from '../../data/team.json';
+import './our_team_styles.css';
+
+const Masters = () => {
+  const [masters, setMasters] = useState([]);
+
+  useEffect(() => {
+    setMasters(teamData);
+  }, []);
+
+  return (
+    <div className="masters-container">
+      <Header />
+      <main>
+        <section className="section_main">
+          <h1 className="h2_text">Наши мастера</h1>
+          <div className="masters">
+            {masters.map((master) => (
+              <div key={master.id} className="master_container">
+                <div className="img">
+                  <img
+                    src={`/images/images_for_our_team/master${master.id}.png`}
+                    alt={master.name}
+                    className="image_master"
+                  />
+                </div>
+                <h2 className="text_under_master">{master.name}</h2>
+                <p className="text">{master.role}</p>
+              </div>
+            ))}
+          </div>
+        </section>
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default Masters;
