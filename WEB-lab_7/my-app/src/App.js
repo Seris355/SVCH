869e75d79e43d0c0285b97e80aa8c4b009e16b39
Seris355 <retyokopka@gmail.com>
2026-01-04 14:10:40 +0300

    fix: delete redux imports

--- WEB-lab_7/my-app/src/App.js
+++ WEB-lab_7/my-app/src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { store } from './store/index.js';
 import Index from './pages/Index/Index.js';
 import Contact from './pages/Contact/Contact.js';
 import Our_team from './pages/Our_team/Our_team.js';
@@ -12,18 +10,16 @@ import './pages/Index/style.css';
 
 function App() {
   return (
-    <Provider store={store}>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/our_team" element={<Our_team />} />
-          <Route path="/service" element={<Service />} />
-          <Route path="/masterclass" element={<MasterClass />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </Router>
-    </Provider>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/our_team" element={<Our_team />} />
+        <Route path="/service" element={<Service />} />
+        <Route path="/masterclass" element={<MasterClass />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
   );
 }
 
