c88b89cb7587ef5347d3bb5cfe937f7c85897eb6
Seris355 <retyokopka@gmail.com>
2026-01-05 03:55:00 +0300

    feat: add routes for admin pages

--- WEB-lab_7/my-app/src/App.js
+++ WEB-lab_7/my-app/src/App.js
@@ -5,7 +5,10 @@ import Contact from './pages/Contact/Contact.js';
 import Our_team from './pages/Our_team/Our_team.js';
 import Service from './pages/Service/Service.js';
 import MasterClass from './pages/MasterClass/MasterClass.jsx';
-import NotFound from './pages/404/404.js'
+import NotFound from './pages/404/404.js';
+import Instructors from './pages/Admin/Instructors/Instructors.js';
+import Participants from './pages/Admin/Participants/Participants.js';
+import MasterClasses from './pages/Admin/MasterClasses/MasterClasses.js';
 import './pages/Index/style.css'; 
 
 function App() {
@@ -17,6 +20,9 @@ function App() {
         <Route path="/our_team" element={<Our_team />} />
         <Route path="/service" element={<Service />} />
         <Route path="/masterclass" element={<MasterClass />} />
+        <Route path="/admin/instructors" element={<Instructors />} />
+        <Route path="/admin/participants" element={<Participants />} />
+        <Route path="/admin/masterclasses" element={<MasterClasses />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
